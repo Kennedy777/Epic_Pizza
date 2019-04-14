99834dc60ef67c17830328e730a759a2bc543b5e
Marguerite Kennedy <reach.marguerite@gmail.com>
2019-04-14 12:47:15 -0700

    fix business logic & HTML name matching

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,38 @@
 // business logifunction Order (sizeSelect, crustType, toppings){
-$(document).ready(function){
-  function Order(size,crust, toppings){
+  function Order(size, crust, toppings){
     this.size=size;
     this.crust=crust;
-    this.toppings=[];
+    this.toppings=toppings;
+    this.basePrice;
   }
 
  Order.prototype.calculateTotal = function(){
   var basePrice = 0;
   if (this.size=="small"){
-     basePrice +=12;
+     this.basePrice +=12;
       }else if(this.size=="medium"){
-     basePrice +=16;
-  } else (this.size=="large"){
-     basePrice +=20;
+     this.basePrice +=16;
+  } else {
+     this.basePrice +=20;
+     for (i=0; i<this.toppings.length; i++){
+       this.basePrice +=2;
   }
-
-for (var i=0; i< this.toppings.length; i++) {
-    basePrice +=2;
-  }
-  return basePrice;
 }
-// };
-
- // Order.prototype.toppingsPrice=function(){
- //   if(this.toppings.length===0){
- //     this.basePrice=basePrice
- //   }else if (this.toppings.length==)
- // }
 
 // Front
-  $("form#Order").click(function(event){
+$(document).ready(function()){
+  $("form#Order").submit(function(event){
     event.preventDefault();
-    var mySize =  $("input[name=size").val();
-    var myCrust = $("#crust").val();
-    var topArray = [];
-     $("input:checkbox[name=toppings]:checked").each(function();
-       topArray.push($(this).val());
+    var mySize =  $("input:radio[name="size"]:checked").val();
+    var myCrust = $("input:radio[name="crust"]:checked").val();
+    var myTops = [];
+    $("input:radio[name="toppings"]:checked").each(function(){
+       myTops.push($(this).val());
+     });
 //for output (confirmation)
-
-    var receipt= new Order(pizzaSize, crustType, toppingsPick);
+    var receipt= new Order(mySize, myCrust, myTops);
+    $("#confirmation").show();
     $(".orderReview").text(receipt);
     $(".finalPrice").text(calculateTotal());
-  )};
+  });
 });
