254e537e4f48849b1040e47f7ef0d2f3d997735e
Marguerite Kennedy <reach.marguerite@gmail.com>
2019-04-13 18:23:59 -0700

    add new colors, hopefully

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,46 @@
-// business logic
-
-var order=function (size, crustType, toppings) {
+// business logifunction Order (sizeSelect, crustType, toppings){
+$(document).ready(function){
+  function Order(size,crust, toppings){
     this.size=size;
     this.crust=crust;
     this.toppings=[];
-}
+  }
 
- Order.prototype.calculateTotal = function() {
+ Order.prototype.calculateTotal = function(){
   var basePrice = 0;
   if (this.size=="small"){
      basePrice +=12;
-  }else if(this.size=="medium"){
-     orderTotal +=16
+      }else if(this.size=="medium"){
+     basePrice +=16;
   } else (this.size=="large"){
-     orderTotal +=20
+     basePrice +=20;
   }
-  };
 
+for (var i=0; i< this.toppings.length; i++) {
+    basePrice +=2;
+  }
+  return basePrice;
+}
+// };
 
-
+ // Order.prototype.toppingsPrice=function(){
+ //   if(this.toppings.length===0){
+ //     this.basePrice=basePrice
+ //   }else if (this.toppings.length==)
+ // }
 
 // Front
-$(document).ready(function(){
-  $("form#Order").click(function(event){
-    var pizzaSize =  $("#size").val();
-    var crustType = $("#crust").val();
-    var toppingsPick = $("input:checkbox[name=toppings]:checked").each(function() {
-    toppingsPrice += 2;
-
-  )};
-});
-
-
-
-
-function Order(crust, crust, size) {
-  this.basePrice = basePrice;
-  this.toppings=option;
-  this.size =size;
-}
-
-
-Order.prototype.calculateTotal = function(total) {
-  size = sizePrice;
-  toppings = [];
-}
-var size= Order.prototype.sizePrice = function(size) {
-if (this.size==small){
-  this.total=10;
-  return this.total;
-}else if(this.size==medium){
-  var price=14;
-  return price;
-}
-
-}
-
-Order.prototype.addToppings = function() {
-
-}
-}
-};
-
-$(document).ready(function(){
   $("form#Order").click(function(event){
     event.preventDefault();
-    var size=$("input:radio[name=size]:checked").val();
-    var topping=$("#topping");
-    var pizza = new Order(size,topping);
-    pizza.addSize(size);
+    var mySize =  $("input[name=size").val();
+    var myCrust = $("#crust").val();
+    var topArray = [];
+     $("input:checkbox[name=toppings]:checked").each(function();
+       topArray.push($(this).val());
+//for output (confirmation)
 
-var toppingsPrice = 0;
-  $("input:checkbox[name=toppings]:checked").each(function() {
-    toppingsCounter += 2;
-  })
-var sizePrice =
-
-var sauce = $("#sauce").val();
-}
-
-};
+    var receipt= new Order(pizzaSize, crustType, toppingsPick);
+    $(".orderReview").text(receipt);
+    $(".finalPrice").text(calculateTotal());
+  )};
 });
