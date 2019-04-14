58b96c41661e3613861c7367063db43f081a6b24
Marguerite Kennedy <reach.marguerite@gmail.com>
2019-04-14 13:54:23 -0700

    new changes to JS and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,40 @@
     this.size=size;
     this.crust=crust;
     this.toppings=toppings;
-    this.basePrice;
+    this.basePrice=0;
   }
 
  Order.prototype.calculateTotal = function(){
-  var basePrice = 0;
   if (this.size=="small"){
      this.basePrice +=12;
       }else if(this.size=="medium"){
      this.basePrice +=16;
   } else {
      this.basePrice +=20;
-     for (i=0; i<this.toppings.length; i++){
+   } for (i=0; i<this.toppings.length; i++){
        this.basePrice +=2;
   }
 }
 
+Order.prototype.confMessage = function() {
+  return "Thank you for your order! Your total will be: $" + this.basePrice;
+};
+
 // Front
-$(document).ready(function()){
+$(document).ready(function(){
   $("form#Order").submit(function(event){
     event.preventDefault();
-    var mySize =  $("input:radio[name="size"]:checked").val();
-    var myCrust = $("input:radio[name="crust"]:checked").val();
+    var mySize =  $("input[name=size]:checked").val();
+    var myCrust = $("input[name=crust]:checked").val();
     var myTops = [];
-    $("input:radio[name="toppings"]:checked").each(function(){
+    $("input[name=toppings]:checked").each(function(){
        myTops.push($(this).val());
-     });
-//for output (confirmation)
-    var receipt= new Order(mySize, myCrust, myTops);
+    });
+
+    var orderConf= new Order(mySize, myCrust, myTops);
+
     $("#confirmation").show();
-    $(".orderReview").text(receipt);
-    $(".finalPrice").text(calculateTotal());
+     $("#orderReview").text(receipt);
+     $("#finalPrice").text(calculateTotal());
   });
 });
